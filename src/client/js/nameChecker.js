ed2b1cc4a68b026ff6659b1315bc5a9f0a25fb2e
berkeleycole <alyssamhope@gmail.com>
2019-07-09 19:18:32 -0700

    all the extras for starting project

---
+++ src/client/js/nameChecker.js
@@ -0,0 +1,14 @@
+function checkForName(inputText) {
+    console.log("::: Running checkForName :::", inputText);
+    let names = [
+        "Picard",
+        "Janeway",
+        "Kirk",
+        "Archer",
+        "Georgiou"
+    ]
+
+    if(names.includes(inputText)) {
+        alert("Welcome, Captain!")
+    }
+}
