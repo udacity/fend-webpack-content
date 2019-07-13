df4eff8042ee06c256bca05a09cfaaab5505c41c
berkeleycole <alyssamhope@gmail.com>
2019-07-13 10:09:41 -0700

    setting up babel

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,6 @@
-import checkForName from './js/nameChecker'
-import formHandler from './js/formHandler'
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+
+console.log(checkForName);
 
 alert("I EXIST")
