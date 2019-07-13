4cc61c6c02d83ce1d1e5fe13eac4e198b5889ee8
berkeleycole <alyssamhope@gmail.com>
2019-07-13 10:12:15 -0700

    webpack loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,14 @@ const webpack = require('webpack')
 
 module.exports = {
     entry: './src/client/index.js',
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    }
 }
