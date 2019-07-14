89d0bb3f7f1f6fbda97aff3c74675a42d0d1423d
berkeleycole <alyssamhope@gmail.com>
2019-07-14 09:56:55 -0700

    adds dev and prod configs

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     entry: './src/client/index.js',
+    mode: 'development',
     devtool: 'source-map',
     module: {
         rules: [
