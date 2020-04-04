2dcccc3adb4836374181dadf0c163b77e28b5f2a
1958gilps <48761839+1958gilps@users.noreply.github.com>
2020-04-04 11:39:46 -0400

    Add files via upload

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,7 @@
-alert("Janny is awesome");
+import { checkForName } from './js/nameChecker'
+
+import { handleSubmit } from './js/formHandler'
+
+console.log(checkForName);
+
+alert("Janny is awesome  !!!!!!!!!!!!!");
--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -1,5 +1,6 @@
-function checkForName(inputText) {
+export function checkForName(inputText) {
     console.log("::: Running checkForName :::", inputText);
+    
     let names = [
         "Picard",
         "Janeway",
