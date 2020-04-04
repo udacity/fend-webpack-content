9ca0da82e22e1613d3cc743908c19b71971bb3a8
1958gilps <48761839+1958gilps@users.noreply.github.com>
2020-04-04 11:49:17 -0400

    Commit before plugins

---
+++ src/client/index.js
@@ -0,0 +1,7 @@
+import { checkForName } from './js/nameChecker'
+
+import { handleSubmit } from './js/formHandler'
+
+console.log(checkForName);
+
+alert("Janny is awesome  !!!!!!!!!!!!!");
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './src/client/index.js',
+    module: {
+        rules: [
+                {
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+                }
+        ]
+      }
+
+}
+
