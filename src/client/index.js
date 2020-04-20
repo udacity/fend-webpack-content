bd2d8cfee1b85920dec0e287b906771ae20a9eef
czyk <aron.scheffczyk@gmail.com>
2020-04-20 13:26:00 +0200

    added babbel to project, set up babbel loader for webpack

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,6 @@
+import { handleSubmit } from './js/formHandler'
+import { checkForName } from './js/nameChecker'
+
+console.log(checkForName);
+
 alert('I exist')
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,22 @@
 const path = require("path")
 const webpack = require('webpack')
 
+
+/**
+ * First rule: 
+ * - test: Regex to select all asset files hat end with .js 
+ * - determints that every .js file needs to be run though the bable-loader
+ */
+
 module.exports = {
     entry: './src/client/index.js',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "bable-loader"
+            }
+        ]
+    }
 }
