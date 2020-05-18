f1d5c3fc7cbf24b6b44325038119f6880fa1f0e9
Niklas Restle <niklas.restle@gmail.com>
2020-05-18 14:37:24 +0300

    Moved index.js into src folder

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/* import { checkForName } from './client/js/nameChecker'
+import { handleSubmit } from './client/js/formHandler'
+ */
+console.log(checkForName);
+
+alert("I EXIST")
+console.log("CHANGE!!");
