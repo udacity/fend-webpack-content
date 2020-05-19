50967bd4c4ef63b429f37508b541ac8a3b7b7e68
Niklas Restle <niklas.restle@gmail.com>
2020-05-19 11:52:49 +0300

    Moved API to .env & installed jest

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,16 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const aylien = require("aylien_textapi")
+const dotenv = require('dotenv');
+dotenv.config();
+
+// API Key
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+  });
+
 
 const app = express()
 
