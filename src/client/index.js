d440d1895934550376a3ced93ef034c646d26e95
Niklas Restle <niklas.restle@gmail.com>
2020-05-19 14:44:15 +0300

    Added CSS plugins and updated client references

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,13 @@
 import { checkForName } from './js/nameChecker.js'
 import { handleSubmit } from './js/formHandler.js'
+
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
 import './styles/resets.scss'
 
-console.log(checkForName);
-
-alert("I EXIST")
-console.log("CHANGE!!");
+export {
+    checkForName,
+    handleSubmit
+}
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,21 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsplugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    output: {
+        libraryTarget: 'var',    
+        library: 'Client'
+    },
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsplugin({})],
+    },
     module: {
         rules: [
             {
@@ -13,13 +24,15 @@ module.exports = {
                 loader: "babel-loader"
             },
             {   test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]}
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]}
         ]
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugin({filename: '[name].css'}),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
