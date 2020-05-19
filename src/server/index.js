c217f8d0736c13a4878fb7a5ed7a168a59e39a14
Niklas Restle <niklas.restle@gmail.com>
2020-05-19 12:18:07 +0300

    Added body-parser and cors packages

--- src/server/index.js
+++ src/server/index.js
@@ -1,20 +1,34 @@
 var path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser') // Middleware
+const cors = require('cors'); // Cross-origin allowance
 const mockAPIResponse = require('./mockAPI.js')
-const aylien = require("aylien_textapi")
+const aylien = require("aylien_textapi") // SDK from Aylien to make API calls
+
+
+
+
+// Use dotenv to process API key from .env file 
 const dotenv = require('dotenv');
 dotenv.config();
 
-// API Key
+
+
+
+
+// API keys via dotenv
 var textapi = new aylien({
     application_id: process.env.API_ID,
     application_key: process.env.API_KEY
   });
 
-
+/* Set up app with middleware */
 const app = express()
-
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 app.use(express.static('dist'))
+
 
 console.log(__dirname)
 
