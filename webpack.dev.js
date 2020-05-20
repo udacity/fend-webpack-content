e6a866731113537ea8df908f41be30390888eae0
Grzegorz (Greg) Ziolo <12894466+21010@users.noreply.github.com>
2020-05-20 23:03:16 +0200

    fix: fix the issue with regex expression in rules
    There is an issue with regex expression - quotes are not required. Documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions

--- webpack.dev.js
+++ webpack.dev.js
@@ -9,7 +9,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
             }
