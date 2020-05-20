daaf0c3c35888d26dc5622eec41f49a96c982dc9
Grzegorz (Greg) Ziolo <12894466+21010@users.noreply.github.com>
2020-05-20 22:52:48 +0200

    Fix: correct the regex expression in rules
    The test in rules is a regex expression so quotes cause the issue and the webpack doesn't work properly.

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,7 +11,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
             }
