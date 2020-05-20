6d8a5f1a95cf014e7111a129f0476347dd73acbd
Grzegorz (Greg) Ziolo <12894466+21010@users.noreply.github.com>
2020-05-20 22:59:11 +0200

    Fix the issue with regex expression
    There is an issue with regex expression. The quotes are not required.

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
             }
