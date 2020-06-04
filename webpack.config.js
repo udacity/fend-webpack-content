cd84df1fc4c30e2f365c69576d7813d94c0f6658
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-06-04 10:11:08 +0530

    html pulins

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const webpack= require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     entry: './src/client/index.js',
@@ -11,5 +12,11 @@ module.exports = {
                 loader: "babel-loader"
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+    ]
 }
