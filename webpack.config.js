8887571f2cfd2f66cf004c89f0ce246ad4c82e9b
Alana Bellucci <fastalana@gmail.com>
2020-07-06 09:41:53 -0600

    1 - Install the html webpack plugin. 2 - Require the plugin in the webpack config. 3 - Add the plugin to the webpack config and instantiate the plugin. 4 - Remove the script from the original index.html file. 5 - Change the index.html file that the server is referencing to the new index.html file that is in the dist folder.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path') 
 const webpack = require('webpack')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: './src/client/index.js',
@@ -11,5 +12,11 @@ module.exports = {
                 loader: "babel-loader"
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
+        })
+    ]
 }
