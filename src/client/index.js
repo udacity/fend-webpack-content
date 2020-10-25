1f665b18a1236d5a1f966e615fe86b7486851af2
Abubakar Siddique <abz.siddique@gmail.com>
2020-10-25 22:56:37 +0000

    Import an image using file-loader

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,9 @@
 import { checkForName } from './js/nameChecker';
 import { handleSubmit } from './js/formHandler';
 
+// Testing image import with "file-loader"
+import img from './images/newspaper.jpg';
+
 console.log(checkForName);
 
 alert("I EXIST");
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,14 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader'
+                  }
+                ]
             }
         ]
     }
