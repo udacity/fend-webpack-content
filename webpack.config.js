f6210f259e1d7af448cc340def664ed048e9f927
Abubakar Siddique <abz.siddique@gmail.com>
2020-11-02 15:48:31 +0000

    refactor: Clean up

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,28 @@ const path = require("path")
 const webpack = require("webpack")
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 module.exports = {
-    entry: './src/client/index.js',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                  {
-                    loader: 'file-loader'
-                  }
-                ]
-            }
-        ]
-    },
-    plugins: [
+  entry: './src/client/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ]
+  },
+  plugins: [
     new HtmlWebPackPlugin({
       template: "./src/client/views/index.html",
       filename: "./index.html",
     })
-    ],
+  ],
 }
