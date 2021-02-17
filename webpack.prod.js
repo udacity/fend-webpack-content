06b26e3db95b172ca1b189c56054a6bd79c97849
Ansgar Hoffmann <ansgar.hoffmann@daimler.com>
2021-02-17 20:44:01 +0100

    dev and prod configs added.

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,7 @@ const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "production",
   entry: "./src/client/index.js",
   module: {
     rules: [
@@ -11,14 +12,6 @@ module.exports = {
         exclude: /node_modules/,
         loader: "babel-loader"
       }
-      //   {
-      //     test: /\.(png|jpe?g|gif)$/i,
-      //     use: [
-      //       {
-      //         loader: "file-loader"
-      //       }
-      //     ]
-      //   }
     ]
   },
   plugins: [
