364cac1cb08efa992cbf36c4b76e9509352ccf15
Ansgar Hoffmann <ansgar.hoffmann@daimler.com>
2021-02-17 20:26:04 +0100

    webpack

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,22 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require("path");
+const express = require("express");
+const mockAPIResponse = require("./mockAPI.js");
 
-const app = express()
+const app = express();
 
-app.use(express.static('src/client'))
+app.use(express.static("dist"));
 
-console.log(__dirname)
+console.log(__dirname);
 
-app.get('/', function (req, res) {
-    res.sendFile('/client/views/index.html', { root: __dirname + '/..' })
-})
+app.get("/", function(req, res) {
+  res.sendFile("dist/index.html");
+});
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+app.listen(8081, function() {
+  console.log("Example app listening on port 8081!");
+});
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.get("/test", function(req, res) {
+  res.send(mockAPIResponse);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
@@ -10,6 +11,20 @@ module.exports = {
         exclude: /node_modules/,
         loader: "babel-loader"
       }
+      //   {
+      //     test: /\.(png|jpe?g|gif)$/i,
+      //     use: [
+      //       {
+      //         loader: "file-loader"
+      //       }
+      //     ]
+      //   }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html"
+    })
+  ]
 };
