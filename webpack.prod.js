dc609cc38331588a7ecb03a7850b8c8caac3db4b
Ansgar Hoffmann <ansgar.hoffmann@daimler.com>
2021-02-18 15:00:34 +0100

    before service workers

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,9 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -13,7 +16,7 @@ module.exports = {
     rules: [
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
       },
       {
         test: "/.js$/",
@@ -26,6 +29,10 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: "./src/client/views/index.html",
       filename: "./index.html"
-    })
-  ]
+    }),
+    new MiniCssExtractPlugin({ filename: "[name].css" })
+  ],
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
+  }
 };
