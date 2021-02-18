b28bf75cae5f42e5e7a41bc52c215545d337812e
Ansgar Hoffmann <ansgar.hoffmann@daimler.com>
2021-02-18 15:26:59 +0100

    service worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -30,7 +31,8 @@ module.exports = {
       template: "./src/client/views/index.html",
       filename: "./index.html"
     }),
-    new MiniCssExtractPlugin({ filename: "[name].css" })
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
+    new WorkboxPlugin.GenerateSW()
   ],
   optimization: {
     minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
