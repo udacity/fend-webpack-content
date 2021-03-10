a51f6a85f2c3799b55fdd7f17c8e996c696b0180
MICHEL <michelrvieira@gmail.com>
2021-03-09 21:11:26 -0300

    Alterações da aula 2 - Item 12

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const webpack = require("webpack")
+const HtmlWebPackPlugin = require("html-webpack-plugin")
 module.exports = {
     entry: './src/client/index.js',
     module: {
@@ -11,4 +12,10 @@ module.exports = {
                 }
         ]
     }
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
 }
