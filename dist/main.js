db6fae6943e6e47c74873232377eaa9c3d7a02b1
MICHEL <michelrvieira@gmail.com>
2021-03-09 22:34:07 -0300

    Exclusão da pasta dist

--- dist/main.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";console.log((function(e){console.log("::: Running checkForName :::",e),["Picard","Janeway","Kirk","Archer","Georgiou"].includes(e)&&alert("Welcome, Captain!")}))})();
