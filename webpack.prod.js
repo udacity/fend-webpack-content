f8ce177eb190d69d0520384f864392ce521d6f89
alilefta <alilefta95@gmail.com>
2021-03-21 18:48:28 +0300

    Webpack mode

---
+++ webpack.prod.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    entry: './src/client/index.js',
+    devtool: 'source-map',
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
+}
