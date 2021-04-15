eabc9d536cf17b3528f2a8db9c9d9c275220f826
Alaa Atef <76438021+alaa-96@users.noreply.github.com>
2021-04-15 13:37:00 +0200

    Update index.js
    Changing the localhost

--- src/server/index.js
+++ src/server/index.js
@@ -13,8 +13,8 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(5005, function () {
+    console.log('Example app listening on port 5005!')
 })
 
 app.get('/test', function (req, res) {
