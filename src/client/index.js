4f32f6cbbbe8e6567994db01545af0f1ca6dc620
Yui-Iida <yxuxi328@icloud.com>
2022-05-12 16:52:54 +0200

    first

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,5 @@
+import {handlesSubmit} from './js/formHandler'
+import {checkForName} from './js/nameChecker'
+
+console.log(checkForName);
 alert("I EXIST")
--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,13 @@ const webpack = require('webpack')
 
 module.exports = {
     entry: './src/client/index.js',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    }
 }
