3a7f63be79b8e86931c40ea8b330fcc4150933b9
Yui-Iida <yxuxi328@icloud.com>
2022-05-13 10:42:39 +0200

    new

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,23 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
 
-const app = express()
+const app = express();
 
-app.use(express.static('src/client'))
+app.use(express.static('dist'));
 
-console.log(__dirname)
+console.log(__dirname);
 
 app.get('/', function (_req, res) {
-    res.sendFile('/client/views/index.html', { root: __dirname + '/..' })
-})
+  //   res.sendFile('/client/views/index.html', { root: __dirname + '/..' });
+  res.sendFile('dist/index.html');
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+  console.log('Example app listening on port 8080!');
+});
 
 app.get('/test', function (_req, res) {
-    res.send(mockAPIResponse)
-})
+  res.send(mockAPIResponse);
+});
