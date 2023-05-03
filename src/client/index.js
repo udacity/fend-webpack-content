caee3ea37267a107889b0513c9ffdab5db3a0e69
devicesandtokens <78351990+devicesandtokens@users.noreply.github.com>
2023-05-03 03:31:48 -0600

    Initial commit

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,25 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+import { checkForName } from './js/nameChecker';
+import { handleSubmit } from './js/formHandler';
+import './styles/resets.scss';
+import './styles/base.scss';
+import './styles/footer.scss';
+import './styles/form.scss';
+import './styles/header.scss';
 
 console.log(checkForName);
 
-alert("I EXIST")
-console.log("CHANGE!!");
+alert('I EXIST');
+console.log('CHANGE!!');
+
+function onBlur() {
+  console.log('Input lost focus');
+}
+
+// Make the onBlur and handleSubmit functions accessible globally
+window.onBlur = onBlur;
+window.handleSubmit = handleSubmit;
+
+export {
+  checkForName,
+  handleSubmit,
+};
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,14 +5,24 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client',
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    },
     module: {
         rules: [
             {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
+              test: /\.js$/,
+              exclude: /node_modules/,
+              use: ["babel-loader"],
+            },
+            {
+              test: /\.scss$/,
+              use: ["style-loader", "css-loader", "sass-loader"],
+            },
+          ]
     },
     plugins: [
         new HtmlWebPackPlugin({
