bdc01e48bd7cda9bdff6e8d4af50aa708ce6b73c
Parminder Kaur <parminderkaur723@gmail.com>
2024-01-18 16:05:36 -0500

    updated files

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+//const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     entry: './src/client/index.js',
@@ -34,5 +35,6 @@ module.exports = {
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
         })
+        //new BundleAnalyzerPlugin()
     ]
 }
